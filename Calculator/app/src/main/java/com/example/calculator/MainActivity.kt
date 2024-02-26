f5a344f7c75c9a24e09fbdea88811c9bec8e2654
sahik1234 <sahikahtesam1994@gmail.com>
2024-02-26 10:39:43 +0600

    Calculator - onClear button functional and ui layout done

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInputText: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvInputText = findViewById(R.id.tvInput)
+    }
+
+    fun onDigit(view: View) {
+        //Toast.makeText(this, "Button Clicked", T oast.LENGTH_SHORT).show()
+        //tvInputText?.append("1") //Append the text when click any button
+
+        //Now, pass view and retrieve the text
+        tvInputText?.append((view as Button).text)
+    }
+
+    fun onClear(view: View){
+        tvInputText?.text = ""
     }
 }
