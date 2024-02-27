2b8bf6a6b44cddc967f9f282747fb43dce68dbf8
sahik1234 <sahikahtesam1994@gmail.com>
2024-02-27 22:02:14 +0600

    Make redy questions data class

--- QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.quizapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var btnStart: Button? = null
+    private var etName: EditText? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnStart = findViewById(R.id.btnStart)
+        etName = findViewById(R.id.etName)
+
+        btnStart?.setOnClickListener {
+            if (etName?.text.toString().isEmpty()) {
+                etName?.error = "Please enter your name"
+                etName?.requestFocus()
+            } else {
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
