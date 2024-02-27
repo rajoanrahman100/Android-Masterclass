89ff3774351a0f7692a1bb6371a876864d47acd3
sahik1234 <sahikahtesam1994@gmail.com>
2024-02-27 23:25:06 +0600

    done with question data model UI

--- QuizApp/app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -2,14 +2,51 @@ package com.example.quizapp
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.quizapp.model.Constant
 
 class QuizQuestionsActivity : AppCompatActivity() {
+
+    private var progressBar: ProgressBar? = null
+    private var tvProgress: TextView? = null
+    private var tvQuestion: TextView? = null
+    private var ivImage: ImageView? = null
+    private var tvOptionOne: TextView? = null
+    private var tvOptionTwo: TextView? = null
+    private var tvOptionThree: TextView? = null
+    private var tvOptionFour: TextView? = null
+    private var buttonSubmit: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
+        progressBar = findViewById(R.id.questionProgressBar)
+        tvProgress = findViewById(R.id.tvProgress)
+        tvQuestion = findViewById(R.id.tvQuestion)
+        ivImage = findViewById(R.id.ivImage)
+        tvOptionOne = findViewById(R.id.tv_option_one)
+        tvOptionTwo = findViewById(R.id.tv_option_two)
+        tvOptionThree = findViewById(R.id.tv_option_three)
+        tvOptionFour = findViewById(R.id.tv_option_four)
+        buttonSubmit = findViewById(R.id.btn_submit)
+
+
+        var questionList = Constant.getQuestions()
+
+        //Print the question size
         Log.i("Question Size", Constant.getQuestions().size.toString())
+
+        for (i in questionList) {
+            ivImage?.setImageResource(i.image)
+            tvQuestion?.text = i.question
+            tvOptionOne?.text = i.optionOne
+            tvOptionTwo?.text = i.optionTwo
+            tvOptionThree?.text = i.optionThree
+            tvOptionFour?.text = i.optionFour
+        }
     }
 }
