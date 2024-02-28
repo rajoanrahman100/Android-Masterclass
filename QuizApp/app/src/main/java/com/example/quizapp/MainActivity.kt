abe805620068bbacb12a91c6fa4e2c176416e029
sahik1234 <sahikahtesam1994@gmail.com>
2024-02-28 08:59:03 +0600

    Question Selection UI Done

--- QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.quizapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -28,4 +29,5 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
 }
