37d60cfce46f069ec89752c90f35a372695ad8c0
sahik1234 <sahikahtesam1994@gmail.com>
2024-03-04 22:37:11 +0600

    Custom Dialg and Custom Progress Dialog

--- Dialogs/app/src/main/java/com/example/dialogs/MainActivity.kt
+++ Dialogs/app/src/main/java/com/example/dialogs/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.dialogs
 
+import android.app.Dialog
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         val btnSnackbar = findViewById<Button>(R.id.snackbar_btn)
         val btnAlertDialog = findViewById<Button>(R.id.alert_dialog_btn)
         val btnCustomDialog = findViewById<Button>(R.id.custom_dialog_btn)
+        val btnCustomProgressDialog = findViewById<Button>(R.id.custom_progress_dialog_btn)
 
         btnSnackbar.setOnClickListener {
             Snackbar.make(it, "Snackbar is clicked", Snackbar.LENGTH_SHORT).show()
@@ -24,7 +27,33 @@ class MainActivity : AppCompatActivity() {
             alertDialogFunction()
         }
 
+        btnCustomDialog.setOnClickListener {
+            customDialogFunction()
+        }
 
+        btnCustomProgressDialog.setOnClickListener {
+            customProgressDialogFunction()
+        }
+
+    }
+
+    private fun customDialogFunction() {
+        val customDialog = Dialog(this)
+        customDialog.setContentView(R.layout.custom_dialog)
+        val submitText = customDialog.findViewById<TextView>(R.id.tv_submit)
+        val cancelText = customDialog.findViewById<TextView>(R.id.tv_cancel)
+
+        submitText.setOnClickListener {
+            Toast.makeText(applicationContext, "clicked submit", Toast.LENGTH_LONG).show()
+            customDialog.dismiss()
+        }
+
+        cancelText.setOnClickListener {
+            Toast.makeText(applicationContext, "clicked cancel", Toast.LENGTH_LONG).show()
+            customDialog.dismiss()
+        }
+
+        customDialog.show()
     }
 
     private fun alertDialogFunction() {
@@ -54,6 +83,17 @@ class MainActivity : AppCompatActivity() {
         // Set other dialog properties
         alertDialog.setCancelable(false) // Will not allow user to cancel after clicking on remaining screen area.
         alertDialog.show() // show the dialog
+    }
+
+    private fun customProgressDialogFunction() {
+        val customProgressDialog = Dialog(this)
+
+        /*Set the screen content from a layout resource.
+        The resource will be inflated, adding all top-level views to the screen.*/
+        customProgressDialog.setContentView(R.layout.custom_progress_dialog)
+
+        //Start the dialog and display it on screen.
+        customProgressDialog.show()
     }
 
 
