b6981877b301d22af0c248819fb82243482c2d49
rifat <rifat@tikweb.dk>
2024-03-05 16:21:01 +0600

    implemented coroutine scope

---
+++ Permission/app/src/main/java/com/example/apppermission/coroutine_scope/CoroutineScop.kt
@@ -0,0 +1,22 @@
+package com.example.apppermission.coroutine_scope
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+
+    println("runBlocking: $this")
+
+    launch {
+        println("launch: $this")
+
+        launch(coroutineContext) {
+            println("child launch: $this")
+        }
+    }
+
+    async {
+        println("async: $this")
+    }
+
+    println("... some other code...")
+}
