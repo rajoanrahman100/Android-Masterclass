f23947e84febe119fc3e844997ec94bd402e2df7
rifat <rifat@tikweb.dk>
2024-03-05 13:15:41 +0600

    cancellable withtimeout builder

---
+++ Permission/app/src/main/java/com/example/apppermission/WIthTimeOutBuilder.kt
@@ -0,0 +1,27 @@
+package com.example.apppermission
+
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
+
+fun main() {
+
+    runBlocking {
+        println("Main Thread Start ${Thread.currentThread().name}")
+
+        withTimeout(2000) {
+            try {
+                for (i in 0..500) {
+                    println("Fake Thread $i ${Thread.currentThread().name}")
+                    delay(300)
+                }
+            }catch (ex:TimeoutCancellationException){
+                println("Exception Caught ${ex.message}")
+            }finally {
+                println("Close resource finally")
+            }
+        }
+        println("Main Thread End ${Thread.currentThread().name}")
+    }
+}
