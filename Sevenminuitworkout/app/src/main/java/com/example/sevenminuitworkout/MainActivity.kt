79c377a0efac57854f9c6922188227fa4eb1b291
sahik1234 <sahikahtesam1994@gmail.com>
2024-03-08 09:07:43 +0600

    start button framelayout design

--- Sevenminuitworkout/app/src/main/java/com/example/sevenminuitworkout/MainActivity.kt
+++ Sevenminuitworkout/app/src/main/java/com/example/sevenminuitworkout/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.sevenminuitworkout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var flStartButton: FrameLayout? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        flStartButton = findViewById(R.id.flStart)
+
+        flStartButton?.setOnClickListener {
+
+        }
+
     }
 }
