6fe4239dae6b20c68a2b2ee81cab0b958853fc8c
sahik1234 <sahikahtesam1994@gmail.com>
2024-03-10 16:40:28 +0600

    adding models and constant

--- Sevenminuitworkout/app/src/main/java/com/example/sevenminuitworkout/Constants.kt
+++ Sevenminuitworkout/app/src/main/java/com/example/sevenminuitworkout/Constants.kt
@@ -4,6 +4,7 @@ object Constants {
     fun getDefaultExerciseList(): ArrayList<ExerciseModel> {
         val exerciseList = ArrayList<ExerciseModel>()
 
+        //Exercises List
         val exercises = listOf<ExerciseModel>(
             ExerciseModel(
                 1, "Jumping Jacks", R.drawable.ic_jumping_jacks, false, false
