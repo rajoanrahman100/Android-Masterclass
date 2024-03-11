3fce626ee567f45de4df46d930a2db4f3ae7013f
sahik1234 <sahikahtesam1994@gmail.com>
2024-03-11 22:10:03 +0600

    implementing github actions

--- Sevenminuitworkout/app/src/main/java/com/example/sevenminuitworkout/ExerciseActivity.kt
+++ Sevenminuitworkout/app/src/main/java/com/example/sevenminuitworkout/ExerciseActivity.kt
@@ -214,6 +214,7 @@ class ExerciseActivity : AppCompatActivity() {
             exerciseProgress = 0
         }
 
+        //Destroy media player sound
         if(mediaPlayer!=null){
             mediaPlayer?.stop()
             mediaPlayer?.release()
