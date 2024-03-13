850c6c0c41799c28a5e7df4d9bdf36a47a82ada2
sahik1234 <sahikahtesam1994@gmail.com>
2024-03-13 23:27:33 +0600

    commenting on codes

--- Sevenminuitworkout/app/src/main/java/com/example/sevenminuitworkout/ExerciseStatusAdapter.kt
+++ Sevenminuitworkout/app/src/main/java/com/example/sevenminuitworkout/ExerciseStatusAdapter.kt
@@ -9,11 +9,20 @@ class ExerciseStatusAdapter(val exerciseItemList: ArrayList<ExerciseModel>) :
     RecyclerView.Adapter<ExerciseStatusAdapter.ExerciseViewHolder>() {
 
 
+    /**
+     * A ViewHolder describes an item view and metadata about its place within the RecyclerView.
+     */
     inner class ExerciseViewHolder(binding: ItemExerciseStatusBinding) :
         RecyclerView.ViewHolder(binding.root) {
         val tvItem=binding.tvItem
     }
 
+    /**
+     * Inflates the item view which is designed in xml layout file
+     *
+     * create a new
+     * {@link ViewHolder} and initializes some private fields to be used by RecyclerView.
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExerciseViewHolder {
         return ExerciseViewHolder(
             ItemExerciseStatusBinding.inflate(
@@ -21,11 +30,23 @@ class ExerciseStatusAdapter(val exerciseItemList: ArrayList<ExerciseModel>) :
             )
         )
     }
-
+    /**
+     * Gets the number of items in the list
+     */
     override fun getItemCount(): Int {
         return exerciseItemList.size
     }
 
+    /**
+     * Binds each item in the ArrayList to a view
+     *
+     * Called when RecyclerView needs a new {@link ViewHolder} of the given type to represent
+     * an item.
+     *
+     * This new ViewHolder should be constructed with a new View that can represent the items
+     * of the given type. You can either create a new View manually or inflate it from an XML
+     * layout file.
+     */
     override fun onBindViewHolder(holder: ExerciseViewHolder, position: Int) {
         val model: ExerciseModel = exerciseItemList[position]
 
