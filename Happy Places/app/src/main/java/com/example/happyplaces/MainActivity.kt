b0fdec154a7affa747558369b31831ab3f399bbc
sahik1234 <sahikahtesam1994@gmail.com>
2024-03-26 20:09:10 +0600

    First commit

--- Happy Places/app/src/main/java/com/example/happyplaces/MainActivity.kt
+++ Happy Places/app/src/main/java/com/example/happyplaces/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.happyplaces
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.happyplaces.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
