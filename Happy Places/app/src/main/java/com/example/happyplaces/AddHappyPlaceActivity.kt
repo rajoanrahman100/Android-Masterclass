1604d26f5619cdef6e3572c6266827ead48093a8
sahik1234 <sahikahtesam1994@gmail.com>
2024-03-31 23:37:47 +0600

    Happy Place Activity Ui done

--- Happy Places/app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
+++ Happy Places/app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
@@ -2,10 +2,24 @@ package com.example.happyplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.happyplaces.databinding.ActivityAddHappyPlaceBinding
+import com.example.happyplaces.databinding.ActivityMainBinding
 
 class AddHappyPlaceActivity : AppCompatActivity() {
+
+    private var binding: ActivityAddHappyPlaceBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_happy_place)
+        binding= ActivityAddHappyPlaceBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.toolbarAddPlace?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+        binding?.toolbarAddPlace?.title="Add Happy Place"
+
+
+
     }
 }
